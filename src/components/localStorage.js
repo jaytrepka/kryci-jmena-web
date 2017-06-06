7e0d26477e6cac25cae3d9a9c87bdfe40e3c8f95
jaytrepka <jaychess17@gmail.com>
2017-06-06 17:20:05 +0200

    Imae

--- src/components/localStorage.js
+++ src/components/localStorage.js
@@ -3,14 +3,12 @@ export function getLocaleData() {
 }
 
 export function setCards(data) {
-  console.log('setC', data);
   const oldData = getLocaleData();
   const updatedData = {...oldData, data};
   localStorage.setItem('KRYCI_JMENA', JSON.stringify(updatedData));
 }
 
 export function setGameId(id) {
-  console.log('setId', id);
   const oldData = getLocaleData();
   const updatedData = {...oldData, game: id};
   localStorage.setItem('KRYCI_JMENA', JSON.stringify(updatedData));
